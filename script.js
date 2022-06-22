9d31a97dddf40748a152b3d238cd3cf369f9fe92
Athul Mathew Varughese <athulmathewvarughese@gmail.com>
2022-06-22 18:01:55 +0530

    new pages where linked to the main indexfile

--- script.js
+++ script.js
@@ -1,5 +1,17 @@
-document.addEventListener('DomContentLoaded',()=>{
-    function algocolor(obj){
-        obj.style.color("red");
-    }
-})
+// document.addEventListener('DomContentLoaded',()=>{
+//     function algocolor(obj){
+//         obj.style.color("red");
+//     }
+// })
+
+function visibility(){
+    document.getElementById("toggle").classList.toggle("visibility")
+    document.getElementById("btn-toggle").classList.toggle("visible")
+
+
+}
+
+
+
+
+
