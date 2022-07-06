a2da7a4a92b7c0ce54f77d533077937b4ac9d24e
Athul Mathew Varughese <athulmathewvarughese@gmail.com>
2022-07-06 12:35:15 +0530

    script updated with cahnnge in preloader

--- script.js
+++ script.js
@@ -1,8 +1,9 @@
-window.onload=async function(){
+window.onload=function(){
     loader();
     generate();
 
 }
+
 
 //  loader class
 async function loader(){
@@ -12,16 +13,7 @@ if(document.getElementById("wizard").style.display=="block"){
         document.getElementById("wizard").style.display="none";
     }
 }
-// function loader(){
-//     var loader = document.getElementById("wiz");
-//     var load = document.getElementById("wizard");
-//     await sleep(2000);
-//     window.addEventListener("load",function(){
-//         loader.style.display="none";
-//         load.style.display="none";
-//     })  
-// }
-
+    
 const arr=[];
 function generateRandom(id){
      element=document.getElementById(id);
@@ -40,9 +32,9 @@ function generate(){
 function visibility(){
     document.getElementById("toggle").classList.toggle("visibility")
     document.getElementById("btn-toggle").classList.toggle("visible")
+
+
 }
-
-
 function sleep(ms){
     return new Promise(resolve=>setTimeout(resolve,ms));
 }
@@ -50,7 +42,8 @@ function sleep(ms){
 async function linearsearch(){
     let key=document.getElementById("textbox").value;
     key=parseInt(key);
-    document.getElementById("textbox").value="";
+    document.getElementById("textbox").value=key;
+
     for(let i=0;i<6;i++){
         let temp=i+1;
         ele=document.getElementById(temp.toString());
@@ -74,31 +67,33 @@ async function linearsearch(){
 function notNeeded(low,mid){
     while(low<=mid){
         let temp=low+1;
-        element.getElementById(temp.toString());
-        element.style.animation="red";
+        const element=document.getElementById(temp.toString());
+        element.style.backgroundColor="red";
         low++;
     }
 }
 
-
 async function binarysearch(){
     let key=document.getElementById("textbox").value;
     key=parseInt(key);
+    console.log(key);
     let low=0;
-    let high=6;
+    let high=5;
     document.getElementById("textbox").value="";
     while(low<=high){
-        let mid=low+(high-low)/2;
+        let mid=Math.floor(low+(high-low)/2);
+        console.log(mid);
         let temp=mid+1;
-        element.getElementById(temp.toString());
+        let element=document.getElementById(temp.toString());
+        console.log(element);
         element.style.backgroundColor="cyan";
         await sleep(2000);
-            if(key==arr[mid]){
-                temp=mid+1;
-                element.getElementById(temp.toString());
-                element.style.backgroundColor="green";
-                break;
-                await sleep(2000)     
+        if(key==arr[mid]){
+            temp=mid+1;
+            element=document.getElementById(temp.toString());
+            element.style.backgroundColor="green";
+            await sleep(2000)
+            break;     
             }else{
                 if(key>arr[mid]){
                     low=mid+1;
@@ -113,11 +108,46 @@ async function binarysearch(){
         }
 }
 
+// selection sort javscript
+async function selectionsort(){
+    for(let i=0;i<5;i++){
+        let temp=i+1;
+        const element=document.getElementById(temp.toString());
+        for(let j=i+1;j<6;j++){
+            let temp1=j+1;
+            const secondelement=document.getElementById(temp1.toString());
+            element.style.backgroundColor="yellow"
+            secondelement.style.backgroundColor="yellow";
+            await sleep(2000);
+            if(arr[i]>arr[j]){
+                element.style.backgroundColor="red"
+                secondelement.style.backgroundColor="red";
+                await sleep(2000);
+                let temp=arr[i];
+                arr[i]=arr[j];
+                arr[j]=temp;
+                element.textContent=arr[i].toString();
+                secondelement.textContent=arr[j].toString();
+                element.style.backgroundColor="green"
+                secondelement.style.backgroundColor="green";
+                await sleep(1500);
+                element.style.backgroundColor="#140A56"
+                secondelement.style.backgroundColor="#140A56";
+                await sleep(2000);
+            }else{
+                element.style.backgroundColor="green"
+                secondelement.style.backgroundColor="green";
+                await sleep(1500);
+                element.style.backgroundColor="#140A56"
+                secondelement.style.backgroundColor="#140A56";
+                await sleep(2000);
 
+            }
+        }
+    }
 
-
-
-
+}
+   
 
 
 
